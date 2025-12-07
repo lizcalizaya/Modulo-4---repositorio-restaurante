534444acd291f2dd0b560ddd0db5d5114e542f04
Owensinho10 <owenleila07@gmail.com>
2025-12-07 00:46:02 -0300

    Update urls.py

--- modulo04_pedidos/urls.py
+++ modulo04_pedidos/urls.py
@@ -43,9 +43,3 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
-
-    path('historial/', pedidos_views.historial_pedidos, name='historial_pedidos'),
-]
-=======
-]
->>>>>>> 67ce92b636632aa62be6ec27ba7a04f024838836
