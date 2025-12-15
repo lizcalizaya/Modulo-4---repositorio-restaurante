ddba64f6a3e1e2a197705b19b7e3494b3d51faf4
Owensinho10 <owenleila07@gmail.com>
2025-12-14 21:17:06 -0300

    Change queryset to return no Pedido objects

--- appPedidos/views.py
+++ appPedidos/views.py
@@ -35,7 +35,7 @@ def notificar_modulo3_listo(pedido: Pedido):
     r.raise_for_status()
 
 class PedidoViewSet(viewsets.ModelViewSet):
-    queryset = Pedido.objects.all().order_by('-fecha_creacion')
+    queryset = Pedido.objects.none()
     serializer_class = PedidoSerializer
 
     transiciones = {
