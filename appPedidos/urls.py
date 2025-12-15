c4e0684d5376a861c9670f5ee0a480cfc2e625d9
Owensinho10 <owenleila07@gmail.com>
2025-12-14 21:28:35 -0300

    Update urls.py

--- appPedidos/urls.py
+++ appPedidos/urls.py
@@ -1,19 +1,11 @@
-# appPedidos/urls.py
 from django.urls import path, include
 from rest_framework import routers
-from .views import PedidoViewSet, estadisticas_tiempos, debug_db
-
+from .views import PedidoViewSet, estadisticas_tiempos
 
 router = routers.DefaultRouter()
 router.register(r'pedidos', PedidoViewSet, basename='pedido')
 
 urlpatterns = [
-    # Endpoints del ViewSet
     path('', include(router.urls)),
-    path("debug-db/", debug_db),
-
-
-    # Endpoint de estadísticas
     path('estadisticas/tiempos/', estadisticas_tiempos, name='estadisticas-tiempos'),
 ]
-
